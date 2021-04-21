38ca9e550d013ff9a4a21b3787badf4628fa01bd
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-04-21 09:09:11 +0100

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { PageFooterComponent } from './page-footer/page-footer.component';
+import { PageHeroComponent } from './page-hero/page-hero.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    PageFooterComponent,
+    PageHeroComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/page-hero/page-hero.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-hero',
+  templateUrl: './page-hero.component.html',
+  styleUrls: ['./page-hero.component.css']
+})
+export class PageHeroComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
