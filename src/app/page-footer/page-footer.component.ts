3570687a41ce503b1df130936d0e959150876992
Isaac Hughes <isaachughesbusiness@gmail.com>
2021-04-23 10:04:58 +0100

    added footer and styled, resized certificates to work with carousel

--- src/app/page-footer/page-footer.component.ts
+++ src/app/page-footer/page-footer.component.ts
@@ -12,4 +12,25 @@ export class PageFooterComponent implements OnInit {
   ngOnInit(): void {
   }
 
+  images = [
+    {path: '../../assets/certification/Capterra.png'},
+    {path: '../../assets/certification/Crown-commercial-service.png'},
+    {path: '../../assets/certification/cyber-essentials.png'},
+    {path: '../../assets/certification/G2-Crowd-Reviews-1.png'},
+    {path: '../../assets/certification/ico-logo.png'},
+    {path: '../../assets/certification/ISO-27001-badge-logo.png'},
+    {path: '../../assets/certification/RapidSSL.png'},
+    {path: '../../assets/certification/secure-trust-logo.png'},
+    {path: '../../assets/certification/UKAS-ISO-9001.png'},
+    {path: '../../assets/certification/Capterra.png'},
+    {path: '../../assets/certification/Crown-commercial-service.png'},
+    {path: '../../assets/certification/cyber-essentials.png'},
+    {path: '../../assets/certification/G2-Crowd-Reviews-1.png'},
+    {path: '../../assets/certification/ico-logo.png'},
+    {path: '../../assets/certification/ISO-27001-badge-logo.png'},
+    {path: '../../assets/certification/RapidSSL.png'},
+    {path: '../../assets/certification/secure-trust-logo.png'},
+    {path: '../../assets/certification/UKAS-ISO-9001.png'}
+  ]
+
 }
